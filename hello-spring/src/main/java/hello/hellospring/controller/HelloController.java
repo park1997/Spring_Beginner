2f4c3450adbd3b0125dc9f1bddcc3353637a439e
park1997 <iambyunghyun97@gmail.com>
2022-06-20 03:43:57 +0900

    Update HelloController.java

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,10 +21,32 @@ public class HelloController {
 
     // 외부에서 Parameter를 받을때
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name,  Model model) {
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
     }
-    
+
+    // API방식 1
+    // 그대로 Data를 내려주는 방식
+    @GetMapping("hello-string")
+    @ResponseBody   // http 프로토콜 body부분에 이 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // API 방식 2
+//    @GetMapping("hello-api")
+//    @ResponseBody
+
+
+    static class Hello {
+        private String name;
+
+        
+
+
+
+
+    }
 
 }
