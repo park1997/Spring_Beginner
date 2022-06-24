6ee3ce66dc1bbe8bb4b2a7ce9ce333bbd328a068
park1997 <iambyunghyun97@gmail.com>
2022-06-24 14:42:58 +0900

    SPRING

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,21 +1,47 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
-    // Autowired : Spring Container에서 가져옴, spring bean에 등록되어있는 객체를 연결해줌
+    // Autowired : Spring Container에서 가져옴, Spring bean에 등록되어있는 객체를 연결해줌
     // DI(Dependency Injection)
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getMyName());
+        memberService.join(member);
+        System.out.println(member.getName());
+        // 회원가입이 끝났으니 홈화면으로 보내버리기
 
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String myName;
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+}
